84a956a78234dbb0927a05223191588934fa43c4
yasas-mintpay <108509570+yasas-mintpay@users.noreply.github.com>
2022-08-13 09:25:01 +0530

    initialize project

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div className={`text-red-600 text-3xl`}>
+          Yo
+        </div>
+    );
+};
+
+export default Index;
