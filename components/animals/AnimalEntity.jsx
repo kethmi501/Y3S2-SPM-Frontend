6f8d7af0c9012b431b6ed547aa6399cee4701488
Osini Kithma <66475844+swee76@users.noreply.github.com>
2022-08-26 20:07:58 +0530

    initial commints in animal entity

---
+++ components/animals/AnimalEntity.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AnimalEntity = () => {
+  return (
+    <div className='px-4 py-8 sm:px-6 sm:py-12 md:px-8 md:py-16 lg:px-10 lg:py-20 bg-white'>AnimalEntity</div>
+  )
+}
+
+export default AnimalEntity
