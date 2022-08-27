4f9e6b3986b701e3985943d56c07449ad1ac1d9a
kethmi <it20021320@my.sliit.lk>
2022-08-27 13:11:55 +0530

    formcomponent-ui

---
+++ pages/widgets/add-tree-widget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddTree from '../../widgets/add-tree/AddTree'
+
+const AddTreeWidget = () => {
+  return (
+    <div className='flex items-center justify-center h-screen bg-blue-50'>
+        <AddTree/>
+    </div>
+  )
+}
+
+export default AddTreeWidget
