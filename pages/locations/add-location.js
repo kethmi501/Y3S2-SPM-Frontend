88e09a20b575ef9b2884c31edb7fd9a3d700aeb8
Sameera <Sameeranicho@gmail.com>
2022-10-08 10:26:37 +0530

    lottie

--- pages/locations/add-location.js
+++ pages/locations/add-location.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import TabGroup from '../../components/treeTabs/TabGroup'
 import AddLocationWidget from '../../widgets/location/AddLocation'
-import {TabMenuLocations} from '../../const/tabMenuLocation'
+import { TabMenuLocations } from '../../const/tabMenuLocation'
+import Background from '../../public/img/lottie/86513-location-forked.json'
+import { Player } from '@lottiefiles/react-lottie-player'
 
 const AddLocation = () => {
   return (
-    <div className="h-screem">
-      <div className="py-6">
-        <TabGroup tabs={TabMenuLocations} current="Add Location" />
-      </div>
-      <div className="px-28">
-        <AddLocationWidget />
+    <div className="h-screen relative">
+      <Player
+        autoplay
+        loop
+        src={Background}
+        style={{
+          width: '100vh',
+          objectFit: 'fill',
+        }}
+      ></Player>
+
+      <div className="absolute w-full top-0">
+        <div className="py-6">
+          <TabGroup tabs={TabMenuLocations} current="Add Location" />
+        </div>
+        <div className="px-28">
+          <AddLocationWidget />
+        </div>
       </div>
     </div>
   )
--- pages/locations/view-location.js
+++ pages/locations/view-location.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import TabGroup from '../../components/treeTabs/TabGroup'
 import ViewLocations from '../../widgets/location/ViewLocations'
-import {TabMenuLocations} from '../../const/tabMenuLocation'
+import { TabMenuLocations } from '../../const/tabMenuLocation'
+import Background from '../../public/img/lottie/29582-looping-idle-location-animation.json'
+import { Player } from '@lottiefiles/react-lottie-player'
 
 const ViewLocation = () => {
   return (
-    <div className="h-screem">
-      <div className="py-6">
-        <TabGroup tabs={TabMenuLocations} current="All Locations" />
+    <div className="h-screen relative">
+      <Player
+        autoplay
+        loop
+        src={Background}
+        style={{
+          width: '100vh',
+          objectFit: 'fill',
+          opacity: '30%',
+        }}
+      ></Player>
+      <div className="absolute w-full top-0  h-full">
+        <div className="py-6">
+          <TabGroup tabs={TabMenuLocations} current="All Locations" />
+        </div>
+        <ViewLocations />
       </div>
-      <ViewLocations />
     </div>
   )
 }
