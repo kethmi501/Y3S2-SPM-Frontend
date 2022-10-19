d14f7aa795be58d3e25013bb6596b9a7803dd817
W.C.Y.S. Lowe <64888128+Flashminat0@users.noreply.github.com>
2022-10-19 21:00:01 +0530

    Update [...slug].js

--- pages/api/[...slug].js
+++ pages/api/[...slug].js
@@ -2,7 +2,7 @@ import httpProxyMiddleware from 'next-http-proxy-middleware'
 
 export default (req, res) =>
   httpProxyMiddleware(req, res, {
-    target: `http://localhost:8000/api/`,
+    target: `https://spm-ser038.herokuapp.com/api/`,
     pathRewrite: [
       {
         patternStr: '^/api',
