93580bdab7922dc69f515513cd6de8944fcecfaa
Sameera <Sameeranicho@gmail.com>
2022-10-20 15:24:22 +0530

    demo

--- widgets/location/UserLocations.jsx
+++ widgets/location/UserLocations.jsx
@@ -15,8 +15,10 @@ const UserLocations = () => {
   useEffect(() => {
     async function getPosts() {
       let response = await retrieveUserPosts()
-      setData(response.result)
-      setLoading(false)
+      if (response) {
+        setData(response.result)
+        setLoading(false)
+      }
     }
     getPosts()
   }, [])
@@ -63,10 +65,7 @@ const UserLocations = () => {
         {data &&
           data.map((location, idx) => (
             <div key={idx} className="relative">
-              <LocationCard
-                locations={location}
-                src={location.images[0]}
-              />
+              <LocationCard locations={location} src={location.images[0]} />
               <div className="flex space-x-2 absolute top-2 left-2">
                 <button
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
